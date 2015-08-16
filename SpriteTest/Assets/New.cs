250f7b42ce9218580e83df9aae24e5eaaff3bcf9
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-08-16 17:28:24 +0500

    Arrow Sprite
    Added Arrow sprite

---
+++ SpriteTest/Assets/New.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class New : MonoBehaviour {
+
+    float startTime;
+
+   Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+
+        Anim = GetComponent<Animator>();
+        Anim.SetBool("Hit", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+        
+        Anim.SetBool("Hit", false);
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            Anim.SetBool("Hit", true);
+           // StartCoroutine(MyCoroutine());
+        
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+
+      
+            Anim.SetBool("Hit", false);
+            // StartCoroutine(MyCoroutine());
+
+        }
+        
+
+
+	}
+    //IEnumerator MyCoroutine()
+    //{
+        
+
+    //   yield WaitForSeconds(0.8f);    //Wait one frame
+    //    Anim.SetBool("Hit", false);
+    //    return 0;
+        
+    //}
+}
